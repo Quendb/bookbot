8a5a064ad9c9ff3ef5477b7b95231fd1f11eea1a
Quendb <quendb@gmail.com>
2025-08-11 21:41:18 +0200

    Completed until report can be printed.

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from stats import calc_num_words, calc_num_chars
-
+from stats import calc_num_words, calc_num_chars, sort_char_dict, print_char_count
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
 def main():
-    total_words = calc_num_words(get_book_text('books/frankenstein.txt'))
-    print(f"{total_words} words found in the document")
-    print(calc_num_chars(get_book_text('books/frankenstein.txt')))
+    file_path = 'books/frankenstein.txt'
+    book_content = get_book_text(file_path)
+    total_words = calc_num_words(book_content)
+    total_chars = calc_num_chars(book_content)
+    # print(f"{total_words} words found in the document")
+    # print(total_chars)
+    sorted_chars = sort_char_dict(total_chars)
+    print(f"============ BOOKBOT ============\n\nAnalyzing book found at {file_path}\n")
+    print(f"----------- Word Count -----------\n\nFound {total_words} total words\n")
+    print_char_count(sorted_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -9,4 +9,23 @@ def calc_num_chars(file_contents):
     for char in file_contents.lower():
         char_dict[char] = char_dict.get(char, 0) + 1
     return char_dict
-        
+
+def sort_on(new_dict_list):
+    ## This function sorts the list of dictionaries by the 'count' key
+    return new_dict_list["num"]
+
+def sort_char_dict(char_dict):
+    ## This function sorts the character dictionary by highest frequency
+    new_dict_list = [{'char':char, 'num':num} for char, num in char_dict.items()]
+    new_dict_list.sort(key=sort_on, reverse=True)
+    return new_dict_list
+
+def print_char_count(new_dict_list):
+    alpha_dict = {}
+    print("----------- Character Count -----------\n")
+    for dict in new_dict_list:
+        if dict['char'].isalpha() == True:
+            alpha_dict[dict['char']] = dict['num']
+    for char, count in alpha_dict.items():
+        print(f"{char}: {count}")
+    
